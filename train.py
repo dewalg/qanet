30a102050327b2d7061ea11f13bde18aadabe359
Dewal Gupta <dewalgupta@gmail.com>
2018-05-26 01:18:47 -0700

    fixed training script

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ MAX_EPOCH = config['hp'].getint('MAX_EPOCH')
 DISPLAY_ITER = config['iter'].getint('DISPLAY_ITER')
 SAVE_ITER = config['iter'].getint('SAVE_ITER')
 VAL_ITER = config['iter'].getint('VAL_ITER')
-SUM_ITER = 100
+SUM_ITER = 250
 
 def average_gradients(tower_grads):
     average_grads = []
@@ -69,7 +69,7 @@ val_init_op = val_it.make_initializer(val_data)
 is_training = tf.placeholder(tf.bool)
 
 experiment = Experiment(api_key="CwfC44eKOZx1oFdva2nDp3P8i", project_name="nlp")
-hyper_params = {"learning_rate": config['hp'].getfloat('LR'), "batch_size": config['dim'].getint('batch_size')}
+hyper_params = {"epochs": MAX_EPOCH, "learning_rate": config['hp'].getfloat('LR'), "batch_size": config['dim'].getint('batch_size')}
 experiment.log_multiple_params(hyper_params)
 
 #build the optimizer
@@ -146,60 +146,75 @@ with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
 
     it = 0
     for epoch in range(MAX_EPOCH):
+        experiment.log_current_epoch(epoch)
         sess.run([train_init_op, val_init_op])
         while True:
-            # run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-            # run_metadata = tf.RunMetadata()
+            try:
+                # run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                # run_metadata = tf.RunMetadata()
 
-            _, loss = sess.run([train_op, avg_loss], feed_dict={is_training: True})
-            experiment.log_metric("loss", loss)
-            # summary_writer.add_run_metadata(run_metadata, 'step001')
-            tf.logging.info('epoch %d, step %d, loss = %f', epoch, it, loss)
-            it += 1
+                _, loss = sess.run([train_op, avg_loss], feed_dict={is_training: True})
+                experiment.log_metric("loss", loss, step=it)
+                # summary_writer.add_run_metadata(run_metadata, 'step001')
+                tf.logging.info('epoch %d, step %d, loss = %f', epoch, it, loss)
+                it += 1
 
-            if it % SUM_ITER:
-                summary_str = sess.run(summary_op, feed_dict={is_training: True})
-                summary_writer.add_summary(summary_str, it)
+                if it % DISPLAY_ITER:
+                    tf.logging.info('step %d, loss = %f', it, loss)
+                    loss_summ = tf.Summary(value=[
+                        tf.Summary.Value(tag="train_loss", simple_value=loss)
+                    ])
+                    summary_writer.add_summary(loss_summ, it)
 
-            if it % VAL_ITER == 0 and it > 0:
-                sess.run(val_init_op)
-                tf.logging.info('validating...')
-                answer_dict = {}
-                losses = []
-                step = 0
-                while step < 157:
-                    try:
-                        _id, vloss, yp1, yp2 = sess.run([qa_id, avg_loss, p1, p2], feed_dict={is_training: False})
+                if it % SUM_ITER:
+                    summary_str = sess.run(summary_op, feed_dict={is_training: True})
+                    summary_writer.add_summary(summary_str, it)
 
-                        tf.logging.info('step %d, val_loss = %f', step, vloss)
-                        answer_dict_, _ = convert_tokens(
-                            dev_eval_f, _id.tolist(), yp1.tolist(), yp2.tolist())
-                        answer_dict.update(answer_dict_)
-                        losses.append(loss)
-                        step += 1
-                    except tf.errors.OutOfRangeError as e:
-                        break
+                if it % VAL_ITER == 0 and it > 0:
+                    sess.run(val_init_op)
+                    tf.logging.info('validating...')
+                    answer_dict = {}
+                    losses = []
+                    step = 0
+                    while step < 157:
+                        try:
+                            _id, vloss, yp1, yp2 = sess.run([qa_id, avg_loss, p1, p2], feed_dict={is_training: False})
 
-                loss = np.mean(losses)
-                metrics = evaluate(dev_eval_f, answer_dict)
-                metrics["loss"] = loss
-                loss_sum = tf.Summary(value=[tf.Summary.Value(
-                    tag="val/loss", simple_value=metrics["loss"]), ])
-                f1_sum = tf.Summary(value=[tf.Summary.Value(
-                    tag="val/f1", simple_value=metrics["f1"]), ])
-                em_sum = tf.Summary(value=[tf.Summary.Value(
-                    tag="val/em", simple_value=metrics["exact_match"]), ])
+                            tf.logging.info('step %d, val_loss = %f', step, vloss)
+                            answer_dict_, _ = convert_tokens(
+                                dev_eval_f, _id.tolist(), yp1.tolist(), yp2.tolist())
+                            answer_dict.update(answer_dict_)
+                            losses.append(loss)
+                            step += 1
+                        except tf.errors.OutOfRangeError as e:
+                            break
 
-                tf.logging.info('val loss: %f', metrics['loss'])
-                tf.logging.info('val f1: %f', metrics['f1'])
-                tf.logging.info('val em: %f', metrics['exact_match'])
+                    loss = np.mean(losses)
+                    metrics = evaluate(dev_eval_f, answer_dict)
+                    metrics["loss"] = loss
+                    loss_sum = tf.Summary(value=[tf.Summary.Value(
+                        tag="val/loss", simple_value=metrics["loss"]), ])
+                    f1_sum = tf.Summary(value=[tf.Summary.Value(
+                        tag="val/f1", simple_value=metrics["f1"]), ])
+                    em_sum = tf.Summary(value=[tf.Summary.Value(
+                        tag="val/em", simple_value=metrics["exact_match"]), ])
 
-                experiment.log_metric("val loss", metrics['loss'])
-                experiment.log_metric("val f1", metrics['f1'])
-                experiment.log_metric("val em", metrics['exact_match'])
-                # add val metrics to summary
-                to_write = [loss_sum, f1_sum, em_sum]
-                for metric in to_write:
-                    summary_writer.add_summary(metric, it)
+                    tf.logging.info('val loss: %f', metrics['loss'])
+                    tf.logging.info('val f1: %f', metrics['f1'])
+                    tf.logging.info('val em: %f', metrics['exact_match'])
 
+                    with experiment.validate():
+                        experiment.log_metric("val loss", metrics['loss'], step=it)
+                        experiment.log_metric("val f1", metrics['f1'], step=it)
+                        experiment.log_metric("val em", metrics['exact_match'], step=it)
+
+                    # add val metrics to summary
+                    to_write = [loss_sum, f1_sum, em_sum]
+                    for metric in to_write:
+                        summary_writer.add_summary(metric, it)
+
+            except tf.errors.OutOfRangeError as e:
+                break
+
+        experiment.log_epoch_end(epoch, it)
     summary_writer.close()
